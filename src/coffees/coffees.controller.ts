21d00455af57553bf1a9a0a91c15eeaa0ed5e93e
Hien Pham <hien.pham@zillearn.com>
2023-01-13 20:34:19 +0700

    implement pagination

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,13 +6,15 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'This returns all coffees';
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This returns all coffees, limit ${limit}, offset ${offset}`;
   }
 
   @Get(':id')
