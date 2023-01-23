17981313d489bf2b32d05483bd49fbd620aca105
Pham Huu Hien <phhien203@gmail.com>
2023-01-23 09:02:12 +0700

    create first e2e test

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,7 +1,7 @@
 import { Injectable, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Event } from 'src/events/entities/event.entity';
+import { Event } from '../events/entities/event.entity';
 import { DataSource } from 'typeorm';
 import coffeesConfig from './coffees.config';
 import { COFFEE_BRANDS } from './coffees.constants';
---
+++ test/coffees/coffees.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeesModule } from '../../src/coffees/coffees.module';
+
+describe('[Feature Coffees] - /coffees', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        CoffeesModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5433,
+          username: 'postgres',
+          password: 'pass123',
+          database: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it.todo('Create [POST /]');
+  it.todo('Get All [GET /]');
+  it.todo('Get One [GET /:id]');
+  it.todo('Update One [PATCH /:id]');
+  it.todo('Delete One [DELETE /:id]');
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
