26fead9dbf99d37039ed3c921ac830942260340c
Pham Huu Hien <phhien203@gmail.com>
2023-01-23 09:02:11 +0700

    install typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      host: 'localhost',
+      type: 'postgres',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true, // disable on PROD
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
