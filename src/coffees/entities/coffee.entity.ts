0c467de852a359f38fe6bbc093b37a587b8be28e
Pham Huu Hien <phhien203@gmail.com>
2023-01-23 09:02:11 +0700

    create relation between entities

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Flavor } from './flavor.entity';
 
 @Entity()
 export class Coffee {
@@ -11,6 +12,6 @@ export class Coffee {
   @Column()
   brand: string;
 
-  @Column('json', { nullable: true })
+  @ManyToMany(() => Flavor, (flavor) => flavor.coffees)
   flavors: string[];
 }
