2516c5c98c69f76e7e3cee6957a64039ea320ec9
Pham Huu Hien <phhien203@gmail.com>
2023-01-23 09:02:11 +0700

    handle update and delete requests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,8 +1,16 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
+  @Get()
   findAll() {
     return 'This returns all coffees';
   }
@@ -15,5 +23,15 @@ export class CoffeesController {
   @Post()
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action update coffee ${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action remove coffee ${id}`;
   }
 }
